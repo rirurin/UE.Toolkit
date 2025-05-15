b30881a7b7979d18980848f7971bde710939496a
RyoTune <zutomayofan50@gmail.com>
2025-05-15 03:08:45 -0500

    Add missing docs to DataTableRow.cs.

--- UE.Toolkit.Reloaded/Common/Types/DataTables/DataTableRow.cs
+++ UE.Toolkit.Reloaded/Common/Types/DataTables/DataTableRow.cs
@@ -5,14 +5,12 @@ namespace UE.Toolkit.Reloaded.Common.Types.DataTables;
 /// <summary>
 /// <see cref="DataTable{TRow}"/> row wrapper for simpler use.
 /// </summary>
-/// <param name="row"></param>
-/// <typeparam name="TRow"></typeparam>
+/// <param name="row">Row instance.</param>
+/// <typeparam name="TRow">Row type.</typeparam>
 public unsafe class DataTableRow<TRow>(TMapElement<FName, TRow>* row)
     where TRow : unmanaged
 {
     public TMapElement<FName, TRow>* Instance { get; } = row;
 
     public string Name { get; } = row->Key.ToString()!;
-
-    public TRow* Value => Instance->Value;
 }

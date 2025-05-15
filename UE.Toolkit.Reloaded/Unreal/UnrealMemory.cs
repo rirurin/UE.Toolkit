bfbaaffb8ae6688caba012e8eabcbcd7b7e06dfb
RyoTune <zutomayofan50@gmail.com>
2025-05-15 03:01:39 -0500

    Add Unreal memory functions.

---
+++ UE.Toolkit.Reloaded/Unreal/UnrealMemory.cs
@@ -0,0 +1,22 @@
+// ReSharper disable InconsistentNaming
+
+namespace UE.Toolkit.Reloaded.Unreal;
+
+public class UnrealMemory
+{
+    internal delegate nint FMemory_Malloc(nint count, int alignment = 0);
+    internal static SHFunction<FMemory_Malloc>? _FMemory_Malloc;
+    
+    internal delegate void FMemory_Free(nint original);
+    internal static SHFunction<FMemory_Free>? _FMemory_Free;
+
+    public UnrealMemory()
+    {       
+        _FMemory_Malloc = new("48 89 5C 24 ?? 57 48 83 EC 20 48 8B F9 8B DA 48 8B 0D ?? ?? ?? ?? 48 85 C9 75 ?? E8");
+        _FMemory_Free = new("48 85 C9 74 ?? 53 48 83 EC 20 48 8B D9 48 8B 0D");
+    }
+    
+    public nint FMemoryMalloc(nint count, int alignment = 0) => _FMemory_Malloc!.Wrapper(count, alignment);
+    
+    public void FMemoryFree(nint original) => _FMemory_Free!.Wrapper(original);
+}

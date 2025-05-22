3c8331ee98987d69ece76db28a93c6ee3f1ddd7a
RyoTune <zutomayofan50@gmail.com>
2025-05-21 21:25:45 -0500

    Cache IsChildOf results. Still not fast enough for GetNativeName use in real-time, but hopefully a bit faster for general use.

--- UE.Toolkit.Core/Types/Unreal/UClass.cs
+++ UE.Toolkit.Core/Types/Unreal/UClass.cs
@@ -5,8 +5,10 @@ namespace UE.Toolkit.Core.Types.Unreal;
 [StructLayout(LayoutKind.Sequential, Size = 0x200)]
 public unsafe struct UClass
 {
-    private static readonly Dictionary<string, EClassCastFlags> KnownCastsMap
+    private static readonly Dictionary<string, EClassCastFlags> CastFlagsMap
         = Enum.GetValues<EClassCastFlags>().ToDictionary(x => x.ToString(), x => x);
+
+    private static readonly Dictionary<CacheKey, bool> CachedResults = [];
         
     public UStruct Super;
     public nint ClassConstructor;
@@ -30,12 +32,19 @@ public unsafe struct UClass
     
     public readonly bool IsChildOf(string type)
     {
-        if (KnownCastsMap.TryGetValue(type, out var castFlag))
+        if (CastFlagsMap.TryGetValue(type, out var castFlag))
         {
             return (ClassCastFlags & castFlag) == castFlag;
         }
+
+        // Caching might use quite a bit of memory, but I'll check that later... probably...
+        var key = new CacheKey(Super.Super.Super.NamePrivate, type);
         
-        return Super.IsChildOf(type);
+        if (CachedResults.TryGetValue(key, out var prevResult)) return prevResult;
+        
+        var result = Super.IsChildOf(type);
+        CachedResults[key] = result;
+        return result;
     }
     
     public readonly bool IsChildOf<T>() => IsChildOf(typeof(T).Name);
@@ -43,4 +52,6 @@ public unsafe struct UClass
     public readonly bool IsA(string type) => IsChildOf(type);
 
     public readonly bool IsA<T>() => IsChildOf(typeof(T).Name);
+
+    private readonly record struct CacheKey(FName ClassFName, string OtherClassName);
 }

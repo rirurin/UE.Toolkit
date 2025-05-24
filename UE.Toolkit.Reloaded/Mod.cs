a2341498775fa9f493052384fe66c5511e4cd1c2
RyoTune <zutomayofan50@gmail.com>
2025-05-23 20:31:56 -0500

    Update mod and add loading object XMLs from mods.

--- UE.Toolkit.Reloaded/Mod.cs
+++ UE.Toolkit.Reloaded/Mod.cs
@@ -3,10 +3,14 @@ using System.Diagnostics;
 #endif
 using Reloaded.Hooks.ReloadedII.Interfaces;
 using Reloaded.Mod.Interfaces;
+using Reloaded.Mod.Interfaces.Internal;
 using UE.Toolkit.Core.Types.Unreal;
 using UE.Toolkit.Interfaces;
+using UE.Toolkit.Interfaces.ObjectWriters;
 using UE.Toolkit.Reloaded.Template;
 using UE.Toolkit.Reloaded.DataTables;
+using UE.Toolkit.Reloaded.ObjectWriters;
+using UE.Toolkit.Reloaded.Toolkit;
 using UE.Toolkit.Reloaded.Unreal;
 
 namespace UE.Toolkit.Reloaded;
@@ -28,6 +32,9 @@ public class Mod : ModBase, IExports
     private readonly UnrealMemory _memory;
     private readonly UnrealObjects _objects;
     private readonly DataTablesService _tables;
+    private readonly TypeRegistry _typeRegistry;
+    private readonly ObjectWriterService _writer;
+    private readonly ToolkitApi _toolkit;
 
     public Mod(ModContext context)
     {
@@ -47,10 +54,27 @@ public class Mod : ModBase, IExports
         _objects = new();
         _tables = new();
         _memory = new();
+        _typeRegistry = new();
+        _writer = new(_typeRegistry, _objects, _tables);
+        _toolkit = new(_writer);
         
         _modLoader.AddOrReplaceController<IUnrealMemory>(_owner, _memory);
         _modLoader.AddOrReplaceController<IDataTables>(_owner, _tables);
         _modLoader.AddOrReplaceController<IUnrealObjects>(_owner, _objects);
+        _modLoader.AddOrReplaceController<ITypeRegistry>(_owner, _typeRegistry);
+        _modLoader.AddOrReplaceController<IToolkit>(_owner, _toolkit);
+        
+        _modLoader.ModLoaded += OnModLoaded;
+    }
+
+    private void OnModLoaded(IModV1 mod, IModConfigV1 modConfig)
+    {
+        var modDir = _modLoader.GetDirectoryForModId(modConfig.ModId);
+        var toolkitDir = Path.Join(modDir, "ue-toolkit");
+        if (!Directory.Exists(toolkitDir)) return;
+
+        var objsDir = Path.Join(toolkitDir, "objects");
+        if (Directory.Exists(objsDir)) _writer.AddPath(objsDir);
     }
 
     #region Standard Overrides
@@ -76,5 +100,5 @@ public class Mod : ModBase, IExports
 
     #endregion
 
-    public Type[] GetTypes() => [typeof(IDataTables), typeof(IUnrealObjects), typeof(UObjectBase)];
+    public Type[] GetTypes() => [typeof(IDataTables), typeof(IUnrealObjects), typeof(IToolkit), typeof(ITypeRegistry), typeof(UObjectBase)];
 }

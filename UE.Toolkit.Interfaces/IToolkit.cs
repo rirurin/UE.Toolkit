1d50cf51a1d9afbb50e6ac2fe9e0cf4315dbd04f
RyoTune <zutomayofan50@gmail.com>
2025-05-25 22:23:54 -0500

    Add to Toolkit API ability to add new UE Toolkit folders.

--- UE.Toolkit.Interfaces/IToolkit.cs
+++ UE.Toolkit.Interfaces/IToolkit.cs
@@ -6,6 +6,12 @@ namespace UE.Toolkit.Interfaces;
 public interface IToolkit
 {
     /// <summary>
+    /// Add a new full UE Toolkit folder, meaning it acts like a mod's <c>ue-toolkit</c>.
+    /// </summary>
+    /// <param name="folder">Folder path.</param>
+    void AddToolkitFolder(string folder);
+    
+    /// <summary>
     /// Add a path to load object XML file(s) from.
     /// </summary>
     /// <param name="path">File or folder path.</param>
--- UE.Toolkit.Reloaded/Toolkit/ToolkitApi.cs
+++ UE.Toolkit.Reloaded/Toolkit/ToolkitApi.cs
@@ -5,5 +5,11 @@ namespace UE.Toolkit.Reloaded.Toolkit;
 
 public class ToolkitApi(ObjectWriterService objWriters) : IToolkit
 {
+    public void AddToolkitFolder(string folder)
+    {
+        var objsDir = Path.Join(folder, "objects");
+        if (Directory.Exists(objsDir)) objWriters.AddPath(objsDir);
+    }
+
     public void AddObjectsPath(string path) => objWriters.AddPath(path);
 }

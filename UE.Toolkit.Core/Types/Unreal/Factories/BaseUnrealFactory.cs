96d2e696c797d5e761d9db25478eb9b08fdd92b7
RyoTune <zutomayofan50@gmail.com>
2025-05-25 20:47:26 -0500

    Add interface and base class for Unreal factories.

---
+++ UE.Toolkit.Core/Types/Unreal/Factories/BaseUnrealFactory.cs
@@ -0,0 +1,71 @@
+using UE.Toolkit.Core.Types.Unreal.Factories.Interfaces;
+
+namespace UE.Toolkit.Core.Types.Unreal.Factories;
+
+public abstract class BaseUnrealFactory : IUnrealFactory
+{
+    public T Cast<T>(IPtr obj)
+    {
+        var typeName = typeof(T).Name;
+        switch (typeName)
+        {
+            case nameof(IUObject):
+                return (T)CreateUObject(obj.Ptr);
+            case nameof(IUClass):
+                return (T)CreateUClass(obj.Ptr);
+            case nameof(IUScriptStruct):
+                return (T)CreateUScriptStruct(obj.Ptr);
+            case nameof(IUEnum):
+                return (T)CreateUEnum(obj.Ptr);
+            case nameof(IUUserDefinedEnum):
+                return (T)CreateUUserDefinedEnum(obj.Ptr);
+            case nameof(IFByteProperty):
+                return (T)CreateFByteProperty(obj.Ptr);
+            case nameof(IFEnumProperty):
+                return (T)CreateFEnumProperty(obj.Ptr);
+            case nameof(IFObjectProperty):
+                return (T)CreateFObjectProperty(obj.Ptr);
+            case nameof(IFSoftClassProperty):
+                return (T)CreateFSoftClassProperty(obj.Ptr);
+            case nameof(IFClassProperty):
+                return (T)CreateFClassProperty(obj.Ptr);
+            case nameof(IFStructProperty):
+                return (T)CreateFStructProperty(obj.Ptr);
+            case nameof(IFMapProperty):
+                return (T)CreateFMapProperty(obj.Ptr);
+            case nameof(IFInterfaceProperty):
+                return (T)CreateFInterfaceProperty(obj.Ptr);
+            case nameof(IFArrayProperty):
+                return (T)CreateFArrayProperty(obj.Ptr);
+            case nameof(IFSetProperty):
+                return (T)CreateFSetProperty(obj.Ptr);
+            case nameof(IFOptionalProperty):
+                return (T)CreateFOptionalProperty(obj.Ptr);
+            default:
+                throw new NotSupportedException(typeName);
+        }
+    }
+
+    public abstract IFProperty CreateFProperty(nint ptr);
+    public abstract IFByteProperty CreateFByteProperty(nint ptr);
+    public abstract IFEnumProperty CreateFEnumProperty(nint ptr);
+    public abstract IFObjectProperty CreateFObjectProperty(nint ptr);
+    public abstract IFSoftClassProperty CreateFSoftClassProperty(nint ptr);
+    public abstract IFClassProperty CreateFClassProperty(nint ptr);
+    public abstract IFStructProperty CreateFStructProperty(nint ptr);
+    public abstract IFMapProperty CreateFMapProperty(nint ptr);
+    public abstract IFInterfaceProperty CreateFInterfaceProperty(nint ptr);
+    public abstract IFArrayProperty CreateFArrayProperty(nint ptr);
+    public abstract IFSetProperty CreateFSetProperty(nint ptr);
+    public abstract IFOptionalProperty CreateFOptionalProperty(nint ptr);
+    public abstract IUObjectArray CreateUObjectArray(nint ptr);
+    public abstract IUObject CreateUObject(nint ptr);
+    public abstract IUClass CreateUClass(nint ptr);
+    public abstract IUScriptStruct CreateUScriptStruct(nint ptr);
+    public abstract IUEnum CreateUEnum(nint ptr);
+    public abstract IUField CreateUField(nint ptr);
+    public abstract IUStruct CreateUStruct(nint ptr);
+    public abstract IUUserDefinedEnum CreateUUserDefinedEnum(nint ptr);
+    public abstract IFFieldClass CreateFFieldClass(nint ptr);
+    public abstract IFField CreateFField(nint ptr);
+}
---
+++ UE.Toolkit.Core/Types/Unreal/Factories/IUnrealFactory.cs
@@ -0,0 +1,33 @@
+using UE.Toolkit.Core.Types.Unreal.Factories.Interfaces;
+
+namespace UE.Toolkit.Core.Types.Unreal.Factories;
+
+/// <summary>
+/// Factory for creating engine-agnostic wrappers around engine-specific implementations.
+/// </summary>
+public interface IUnrealFactory
+{
+    T Cast<T>(IPtr obj);
+    IFProperty CreateFProperty(nint ptr);
+    IFByteProperty CreateFByteProperty(nint ptr);
+    IFEnumProperty CreateFEnumProperty(nint ptr);
+    IFObjectProperty CreateFObjectProperty(nint ptr);
+    IFSoftClassProperty CreateFSoftClassProperty(nint ptr);
+    IFClassProperty CreateFClassProperty(nint ptr);
+    IFStructProperty CreateFStructProperty(nint ptr);
+    IFMapProperty CreateFMapProperty(nint ptr);
+    IFInterfaceProperty CreateFInterfaceProperty(nint ptr);
+    IFArrayProperty CreateFArrayProperty(nint ptr);
+    IFSetProperty CreateFSetProperty(nint ptr);
+    IFOptionalProperty CreateFOptionalProperty(nint ptr);
+    IUObjectArray CreateUObjectArray(nint ptr);
+    IUObject CreateUObject(nint ptr);
+    IUClass CreateUClass(nint ptr);
+    IUScriptStruct CreateUScriptStruct(nint ptr);
+    IUEnum CreateUEnum(nint ptr);
+    IUField CreateUField(nint ptr);
+    IUStruct CreateUStruct(nint ptr);
+    IUUserDefinedEnum CreateUUserDefinedEnum(nint ptr);
+    IFFieldClass CreateFFieldClass(nint ptr);
+    IFField CreateFField(nint ptr);
+}

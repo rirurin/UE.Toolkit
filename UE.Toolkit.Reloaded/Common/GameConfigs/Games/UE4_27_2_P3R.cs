db495659eb5b659a89eca142ea21dab399f3f4d5
RyoTune <zutomayofan50@gmail.com>
2025-06-17 01:38:04 -0500

    Remove patterns from game configs.

--- UE.Toolkit.Reloaded/Common/GameConfigs/Games/UE4_27_2_P3R.cs
+++ UE.Toolkit.Reloaded/Common/GameConfigs/Games/UE4_27_2_P3R.cs
@@ -1,4 +1,3 @@
-using UE.Toolkit.Core.Common;
 using UE.Toolkit.Core.Types.Unreal.Factories;
 using UE.Toolkit.Core.Types.Unreal.Factories.UE4_27_2;
 // ReSharper disable InconsistentNaming
@@ -8,11 +7,5 @@ namespace UE.Toolkit.Reloaded.Common.GameConfigs.Games;
 public class UE4_27_2_P3R : UE5_4_4_ClairObscur
 {
     public override string Id => "P3R";
-    public override string UObject_PostLoadSubobjects => "40 53 48 83 EC 20 48 8B 41 ?? 48 8B D9 8B 90 ?? ?? ?? ?? C1 EA 17";
-    public override string GUObjectArray => "48 8B 05 ?? ?? ?? ?? 48 8B 0C ?? 48 8D 04 ?? 48 85 C0 74 ?? 44 39 40 ?? 75 ?? F7 40 ?? 00 00 00 30 75 ?? 48 8B 00";
-    public override Func<nint, nint> GUObjectArray_Result => result => ToolkitUtils.GetGlobalAddress(result + 3) - 0x10;
-    public override string UStruct_IsChildOf => "48 85 D2 74 ?? 48 63 42 ?? 4C 8D 42 ??";
-    public override string GFNamePool => "4C 8D 05 ?? ?? ?? ?? EB ?? 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 4C 8B C0 C6 05 ?? ?? ?? ?? 01 48 8B 44 24 ?? 48 8B D3 48 C1 E8 20 8D 0C ?? 49 03 4C ?? ?? E8 ?? ?? ?? ?? 48 8B C3";
-    public override string UEnum_GetDisplayNameTextByIndex => "48 89 5C 24 ?? 55 56 57 48 83 EC 30 48 8B FA 41 8B E8";
     public override IUnrealFactory Factory { get; } = new UnrealFactory();
 }
--- UE.Toolkit.Reloaded/Common/GameConfigs/IGlobalConfig.cs
+++ UE.Toolkit.Reloaded/Common/GameConfigs/IGlobalConfig.cs
@@ -7,17 +7,5 @@ namespace UE.Toolkit.Reloaded.Common.GameConfigs;
 public interface IGameConfig
 {
     string Id { get; }
-    string UObject_PostLoadSubobjects { get; }
-    string GUObjectArray { get; }
-    Func<nint, nint> GUObjectArray_Result { get; }
-    string UStruct_IsChildOf { get; }
-    string GFNamePool { get; }
-    string FNameHelper_FindOrStoreString { get; }
-    //string FMemory_Malloc { get; }
-    //string FMemory_Free { get; }
-    string GMalloc { get; }
-    string UDataTable_HandleDataTableChanged { get; }
-    string UEnum_GetDisplayNameTextByIndex { get; }
-    string FName_Ctor_Wide { get; }
     IUnrealFactory Factory { get; }
 }
